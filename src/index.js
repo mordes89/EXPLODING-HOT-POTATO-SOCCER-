73dee4fd9ce16bf9c9bc9a1b3fd29272e2f9b5dd
mordes89 <mordes89@gmail.com>
2021-08-14 16:45:28 -0400

    Got multidirectional pad to move

--- src/index.js
+++ src/index.js
@@ -7,39 +7,98 @@
 //    // new Example(main);
 // }); 
 const canvas = document.querySelector('canvas'); //find the <canvas> element in the HTML
-// canvas.width = window.innerWidth;
-// canvas.hight = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');            // Conventionally, ctx defined as our 2d canvas
+requestAnimationFrame(play); //Loop gameplay
+// User
+let user = {
+   x: 20,
+   y: 30,
+   w: 50,
+   h: 20,
+   move: 10
+}
+
+document.addEventListener("keydown", keyDowns);
+document.addEventListener("keyup", keyUps);
 
 
-document.addEventListener("keydown", function(){
+
+function keyDowns(){
    if (event.code ==='ArrowLeft'){
-      console.log("ArrowLeft")
+      user.x -= user.move;
+      console.log(event.code)
    }
    if (event.code ==='ArrowRight'){
-      console.log("ArrowRight")
+      user.x += user.move;
+      console.log(event.code)
    }
    if (event.code ==='ArrowDown'){
-      console.log("ArrowDown")
+      user.y += user.move;
+      console.log(event.code)
    }
    if (event.code ==='ArrowUp'){
-      console.log("ArrowUp")
+      user.y -= user.move;
+      console.log(event.code)
    }
-});
+}
 
-// Field
-ctx.fillStyle = 'green';
-ctx.fillRect(40, 40, 200, 100);
-// Left goal
-ctx.fillStyle = 'white';
-ctx.fillRect(20, 70, 20, 40);
-// Right goal
-ctx.fillStyle = 'white';
-ctx.fillRect(240, 70, 20, 40);
-// Right goal
-ctx.fillStyle = 'white';
-ctx.fillRect(240, 70, 20, 40);
-// ^^ctx.fillRect(x, y, Width, Hight); ([x=0, y=0] is the top left corner)
+function keyUps(){
+   if (event.code ==='ArrowLeft'){
+      user.x -= user.move;
+      console.log(event.code)
+   }
+   if (event.code ==='ArrowRight'){
+      user.x += user.move;
+      console.log(event.code)
+   }
+   if (event.code ==='ArrowDown'){
+      user.y += user.move;
+      console.log(event.code)
+   }
+   if (event.code ==='ArrowUp'){
+      user.y -= user.move;
+      console.log(event.code)
+   }
+}
+
+function play(){
+   // Clear the board before making a move:
+   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+
+   // User's new position printed:
+   ctx.fillStyle = 'red';
+   ctx.fillRect(user.x, user.y, user.w, user.h);
+
+   // Continue playing:
+   requestAnimationFrame(play); //Loop gameplay
+
+}
+
+
+
+
+
+// // Field
+// ctx.fillStyle = 'green';
+// ctx.fillRect(40, 40, 200, 100);
+// // Left goal
+// ctx.fillStyle = 'white';
+// ctx.fillRect(20, 70, 20, 40);
+// // Right goal
+// ctx.fillStyle = 'white';
+// ctx.fillRect(240, 70, 20, 40);
+// // Right goal
+// ctx.fillStyle = 'white';
+// ctx.fillRect(240, 70, 20, 40);
+
+
+
+
+
+
+
 
 // function animate() { //Recursive function that    
 //    ctx.clearRect(0, 0, innerWidth, innerHeight);
