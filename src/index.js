acf3146ef494ba9610b1d48475f85850cc97d8e6
mordes89 <mordes89@gmail.com>
2021-08-16 13:30:46 -0400

    ability to aim ball and shoot with power

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ requestAnimationFrame(play); //Loop gameplay
 function play(){   
    moveUser();
    moveBall();
+   aimBall();
+   
+   
 
    // Clear the board before making a move:
    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
@@ -34,10 +37,16 @@ function play(){
    printgoalRight()
    printpenBoxL();
    printpenBoxR();
-   // print user and ball:
-   printSprite(userSprite, user.dpW * user.dpFrameX, user.dpH * user.dpFrameY, user.dpW, user.dpH, user.x, user.y, canvas.width/10, canvas.height/6)
+   // print user, ai and ball:
+   printSprite(userSprite, user.W * user.FrameX, user.H * user.FrameY, user.W, user.H, user.x, user.y, canvas.width/10, canvas.height/6)
+   printSprite(aiSprite, ai.W * ai.FrameX, ai.H * ai.FrameY, ai.W, ai.H, ai.x, ai.y, canvas.width/10, canvas.height/6)
    printBall(); 
-   // printUser();   
+   if (ballPossession) {      
+      printBallAimDots(ball.x+(vx*2), ball.y+(vy*3), 0.2, 5)        
+      printBallAimDots(ball.x+(vx*4), ball.y+(vy*5), 0.2, 3)        
+      printBallAimDots(ball.x+(vx*6), ball.y+(vy*7), 0.2, 1)        
+   }
+
    setTimeout(function(){
       // Continue playing recursively:
       requestAnimationFrame(play); //Loop gameplay
@@ -49,48 +58,45 @@ let continuousLeft = false;
 let continuousRight = false;
 let continuousDown = false;
 let continuousUp = false;
+let continuousD = false;
 let continuousS = false;
 let continuousW = false;
 
 function keyDowns(){
    if (event.code ==='ArrowLeft'){
       continuousLeft = true
-      if (vx>0 && (ballPosesion === true)) {         
+      if (vx>0 && (ballPossession === true)) {         
          vx= -(vx);
       }
       // console.log(event.code)
    } else if (event.code ==='ArrowRight'){
       continuousRight = true
-      if (vx<0 && (ballPosesion === true)) {         
+      if (vx<0 && (ballPossession === true)) {         
          vx= -(vx);
       // console.log(event.code)
       }
    }
    if (event.code ==='ArrowDown'){
       continuousDown = true  
-      if (vy<0 && (ballPosesion === true)) {         
+      if (vy<0 && (ballPossession === true)) {         
          vy= -(vy);
       }   
    } else if (event.code ==='ArrowUp'){
       continuousUp = true
-      if (vy>0 && (ballPosesion === true)) {         
+      if (vy>0 && (ballPossession === true)) {         
          vy = -(vy);
       }
    }
 
-   if (event.code ==='KeyD'){  //Kick Ball
-      ballPosesion = false  
-      ballRolling = true    
-      // console.log(event.code)
+   if (event.code ==='KeyD'){  //Kick Ball        
+      continuousD = true;      // hold down D for strength
+      vx = 2;
    }
-
    if (event.code ==='KeyS'){  //Aim kick down
-      continuousS = true;
-      vy--;
+      continuousS = true;          
    }
    if (event.code ==='KeyW'){  //Aim kick up
-      continuousW = true;
-      vy++;
+      continuousW = true;      
    }
 }
 
@@ -109,6 +115,47 @@ function keyUps(){
       continuousUp = false  
       // console.log(event.code)
    }  
+
+   if (event.code ==='KeyD'){  //Kick Ball
+      ballPossession = false  
+      ballRolling = true   
+      continuousD = false
+   }
+   if (event.code ==='KeyS'){  //Aim kick down
+      continuousS = false;          
+   }
+   if (event.code ==='KeyW'){  //Aim kick up
+      continuousW = false;      
+   }
+}
+
+function aimBall(){
+   if (continuousS && ballPossession){  //Aim kick down
+      if (vy<15) {         
+         vy++;
+         // console.log(vy);
+      }  
+   }
+   if (continuousW && ballPossession){  //Aim kick up
+      if (vy>-15) {         
+         vy--;
+         // console.log(vy);
+      }  
+   }
+   if (continuousD && ballPossession){  
+      if (vx<15) {         
+         vx++;
+         console.log(vx);
+      }  
+   }
+}
+
+function printBallAimDots(x, y, radius, width){
+   ctx.beginPath();
+   ctx.lineWidth = width;
+   ctx.strokeStyle = "red"
+   ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+   ctx.stroke();   
 }
 
 
@@ -118,17 +165,30 @@ let user = {
    y: canvas.height/2-canvas.height/10,
    // w: canvas.width/50,
    // h: canvas.height/10,
-   dpW: 40,
-   dpH: 56,
-   dpFrameX: 0,
-   dpFrameY: 2,
+   W: 40,
+   H: 56,
+   FrameX: 0,
+   FrameY: 2,
+   steps: 5
+}
+
+let ai = {
+   x: canvas.width - canvas.width/10,
+   y: canvas.height/2-canvas.height/10,
+   // w: canvas.width/50,
+   // h: canvas.height/10,
+   W: 32,
+   H: 48,
+   FrameX: 0,
+   FrameY: 1,
    steps: 5
 }
 
 const userSprite = new Image();
-userSprite.src = "./pics/hulk.png"; //dpW: 40, dpH: 56,
-// userSprite.src = "./pics/deadpool.png"; //dpW: 32, dpH: 48,
-// userSprite.src = "./pics/yoda.png"; //dpW: 32, dpH: 48,
+userSprite.src = "./pics/hulk.png"; //W: 40, H: 56,
+const aiSprite = new Image();
+aiSprite.src = "./pics/deadpool.png"; //W: 32, H: 48,
+// userSprite.src = "./pics/yoda.png"; //W: 32, H: 48,
 
 function printSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH)
@@ -136,88 +196,86 @@ function printSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
 
 // function printUser(){
 //    ctx.fillStyle = 'red';
-//    ctx.fillRect(user.x, user.y, user.dpw, user.dpW);
+//    ctx.fillRect(user.x, user.y, user.w, user.W);
 // }
 function moveUser(){
    if (continuousLeft === true && user.x - user.steps > canvas.width/50){  
       user.x -= user.steps;   // move in direction
-      user.dpFrameY = 1;      // character frame row 1
+      user.FrameY = 1;      // character frame row 1
       userMovingPics();       // alternate pice in row
-      if (ballPosesion) {      //runs to the left with ball 
+      if (ballPossession) {      //runs to the left with ball 
          ball.x = user.x+20;
-         ball.y = user.y+user.dpH+10;
+         ball.y = user.y+user.H+20;
       }
    }
-   if (continuousRight === true && user.x + user.steps < (canvas.width/2)-user.dpW){
+   if (continuousRight === true && user.x + user.steps < (canvas.width/2)-user.W){
       user.x += user.steps;
-      user.dpFrameY = 2;
+      user.FrameY = 2;
       userMovingPics();
-      if (ballPosesion) {      //runs to the right with ball 
-         ball.x = user.x+user.dpW+10;
-         ball.y = user.y+user.dpH+10;
+      if (ballPossession) {      //runs to the right with ball 
+         ball.x = user.x+user.W+10;
+         ball.y = user.y+user.H+20;
       }
    }
-   if (continuousDown === true && user.y + user.steps < canvas.height-user.dpW){
+   if (continuousDown === true && user.y + user.steps < canvas.height-user.W){
       user.y += user.steps;
-      user.dpFrameY = 0;
+      user.FrameY = 0;
       userMovingPics();
-      if (ballPosesion) {      //runs down with ball 
+      if (ballPossession) {      //runs down with ball 
          ball.x = user.x+30;
-         ball.y = user.y+70;
+         ball.y = user.y+80;
       }
    }
    if (continuousUp === true && user.y - user.steps > canvas.height/50){
       user.y -= user.steps;
-      user.dpFrameY = 3;
+      user.FrameY = 3;
       userMovingPics();
-      if (ballPosesion) {      //runs up with ball 
-         ball.x = user.x+80;
+      if (ballPossession) {      //runs up with ball 
+         ball.x = user.x+60;
          ball.y = user.y+60;
       }
-   }
+   }   
 }
 
 function userMovingPics() {   
-   if (user.dpFrameX < 3) {
-      user.dpFrameX++;
+   if (user.FrameX < 3) {
+      user.FrameX++;
    } else {
-      user.dpFrameX = 0;
+      user.FrameX = 0;
    }
 }
-
-
-
 
 
 // Ball
 let ball = {
-   x: user.x+user.dpW+10,
-   y: user.y+user.dpH+10,
+   x: user.x+user.W+10,
+   y: user.y+user.H+10,
    radius: window.innerWidth/170,
 }
 function printBall(){
    ctx.beginPath();
-   ctx.lineWidth = 6;
-   ctx.strokeStyle = "yellow"
    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2, false);
+   ctx.lineWidth = 7;
+   ctx.strokeStyle = "yellow"
    ctx.stroke();   
-   // ctx.fill(); 
+   ctx.fillStyle = "blue"
+   ctx.fill(); 
 }
 
 
 let vx = 7;
 let vy = 7;
 let ballRolling = true;
-let ballPosesion = false;
+let ballPossession = false;
 function moveBall(){
    if (ballRolling) {      
       ball.x += vx;
       ball.y += vy;
    }
-   // player takes control
-   if ((ball.x <= user.x+user.dpW && ball.x >= user.x) && (ball.y >= user.y && ball.y <= (user.y + user.dpH))) {
+   // player takes control of ball
+   if ((ball.x <= user.x+user.W && ball.x >= user.x) && (ball.y >= user.y && ball.y <= (user.y + user.H+30))) {
       ballRolling = false;
-      ballPosesion = true;
+      ballPossession = true;
       vy = 0.1;
    }
    
@@ -255,7 +313,7 @@ let fieldMidCircle = {
    x: canvas.width/2,
    y: canvas.height/2,   
    radius: canvas.height/10,
-   width: canvas.width/80
+   width: canvas.width/140
 }
 function printMidCir(){
    ctx.beginPath();
@@ -267,9 +325,9 @@ function printMidCir(){
 }
 
 let midLine = {
-   x: ((canvas.width/2) - canvas.width/70/2),
+   x: ((canvas.width/2) - canvas.width/140/2),
    y: 0,
-   w: canvas.width/70,
+   w: canvas.width/140,
    h: canvas.height,
 }
 function printMidLine(){
@@ -372,12 +430,6 @@ function printpenBoxRhalfCircle(){
    ctx.arc(penBoxRhalfCircle.x, penBoxRhalfCircle.y, penBoxRhalfCircle.radius, 1.5, -1.5, false);
    ctx.stroke();   
 }
-// let penBoxDot = {
-   // x: penBoxR.lBoxX+30,
-   // y: penBoxR.lBoxY*3.5,   
-   // radius: 1,
-   // width: penBoxL.verticalW
-// }
 
 function printpenBoxR(){
    ctx.fillStyle = 'white';
@@ -392,6 +444,7 @@ function printpenBoxR(){
 }
 
 
+// print goals
 let goalLeft = {
    x: 0,
    y: canvas.height/3,
